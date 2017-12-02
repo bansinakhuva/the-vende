d6d11b4da0474e0d6f0140200bc6ef483c74507f
Bansi <33387973+cptmoss11@users.noreply.github.com>
2017-12-01 17:06:00 -0800

    fbconfig

--- fb_config.php
+++ fb_config.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-$appId = '466264270413471';
+$appId = '440948386100123';
 $fb = new Facebook\Facebook([
 	'app_id' => $appId,
-	'app_secret' => 'e5f6c62093d5e7532f8e3629150c7b81',
-	'default_graph_version' => 'v2.11',
+	'app_secret' => '312ca3c39c54e75c4d2932a2f38fdeb8',
+	'default_graph_version' => 'v2.5',
 ]);
 ?>
